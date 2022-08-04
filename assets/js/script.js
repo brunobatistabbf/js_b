2f0851d23d4824c6228706bf52389ce3e1363bcf
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2022-08-03 23:41:18 -0300

    first ecent code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-let String = "bruno"
-let number = 90.0
-let boolean = true
-let lista = ["ovo", "Ola" ]
-let nome = {nome:"bonieky", idade :19.0}
-//Comentario
+function clique() {
+    alert("Você clicou no botão")
+}
